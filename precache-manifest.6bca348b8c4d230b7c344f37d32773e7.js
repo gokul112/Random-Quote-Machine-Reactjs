7c8631912fa38caf7913a925f68b596b81dbb519
gokul raj <gokul.raj@gigamon.com>
2020-04-15 17:00:37 +0530

    Updates

---
+++ precache-manifest.6bca348b8c4d230b7c344f37d32773e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abdaf22e1843f9e726f0536fb8c756d",
+    "url": "/Random-Quote-Machine-Reactjs/index.html"
+  },
+  {
+    "revision": "b61475773695592f3f9e",
+    "url": "/Random-Quote-Machine-Reactjs/static/css/main.0c6bd6c6.chunk.css"
+  },
+  {
+    "revision": "eec44c57c0bad85dfedc",
+    "url": "/Random-Quote-Machine-Reactjs/static/js/2.fb01ca35.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Random-Quote-Machine-Reactjs/static/js/2.fb01ca35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61475773695592f3f9e",
+    "url": "/Random-Quote-Machine-Reactjs/static/js/main.276daf8d.chunk.js"
+  },
+  {
+    "revision": "5f2e5e04dbbf69842eab",
+    "url": "/Random-Quote-Machine-Reactjs/static/js/runtime-main.dfe1f47c.js"
+  }
+]);
